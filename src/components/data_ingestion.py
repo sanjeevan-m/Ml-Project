d204b6fa14748a7fd68b0da09f4204b1200e995e
sanjeevan-m <sanjeevanwarrior@gmail.com>
2023-04-04 22:59:15 +0530

    model traner

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,45 +1,64 @@
 import os
-import sys 
-from src.logger import logging
+import sys
 from src.exception import CustomException
-import pandas as pd 
+from src.logger import logging
+import pandas as pd
+
 from sklearn.model_selection import train_test_split
-from dataclasses import dataclass 
+from dataclasses import dataclass
 
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
 @dataclass
-class dataingestionconfig:
-    train_dat_path:str=os.path.join("artifacts","train.csv")
-    test_dat_path:str=os.path.join("artifacts","test.csv")
-    raw_dat_path:str=os.path.join("artifacts","data.csv")
+class DataIngestionConfig:
+    train_data_path: str=os.path.join('artifacts',"train.csv")
+    test_data_path: str=os.path.join('artifacts',"test.csv")
+    raw_data_path: str=os.path.join('artifacts',"data.csv")
 
-class dataingestion:
+class DataIngestion:
     def __init__(self):
-        self.ingestion_config=dataingestionconfig()
+        self.ingestion_config=DataIngestionConfig()
+
     def initiate_data_ingestion(self):
-        logging.info("entered the data ingestion method")
+        logging.info("Entered the data ingestion method or component")
         try:
-            data=pd.read_csv("notebook\std.csv")
-            logging.info("read data")
+            df=pd.read_csv('notebook\std.csv')
+            logging.info('Read the dataset as dataframe')
 
-            os.makedirs(os.path.dirname(self.ingestion_config.train_dat_path),exist_ok=True)
-            data.to_csv(self.ingestion_config.raw_dat_path,index=False,header=True)
-            logging.info("split data initated")
-            train_set,test_set=train_test_split(data,test_size=0.2,random_state=42)
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
 
-            train_set.to_csv(self.ingestion_config.train_dat_path,index=False,header=True)
-            test_set.to_csv(self.ingestion_config.test_dat_path,index=False,header=True) 
-            logging.info("ingestion of data completed")
+            df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
+
+            logging.info("Train test split initiated")
+            train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
+
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
+
+            logging.info("Inmgestion of the data iss completed")
+
             return(
-                self.ingestion_config.train_dat_path,
-                self.ingestion_config.test_dat_path
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+
             )
         except Exception as e:
-            raise CustomException(e,sys) 
-
+            raise CustomException(e,sys)
+        
 if __name__=="__main__":
-    obj=dataingestion()
-    obj.initiate_data_ingestion()
+    obj=DataIngestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initate_model_trainer(train_arr,test_arr))
+
 
 
 
---
+++ src/components/model_trainer.py
@@ -0,0 +1,114 @@
+import os
+import sys
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt 
+import seaborn as sns
+from dataclasses import dataclass
+# Modelling
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor,AdaBoostRegressor
+from sklearn.svm import SVR
+from sklearn.linear_model import LinearRegression, Ridge,Lasso
+from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
+from sklearn.model_selection import RandomizedSearchCV
+#from catboost import CatBoostRegressor
+#from xgboost import XGBRegressor
+import warnings
+from src.logger import logging
+from src.exception import CustomException 
+from src.utills import save_object,evaluate_models
+
+@dataclass 
+class ModelTrainerConfig:
+    trained_model_file_path=os.path.join("artifacts","model.pk1")
+
+class ModelTrainer:
+    def __init__(self):
+        self.model_trainer_config=ModelTrainerConfig()
+
+    def initate_model_trainer(self,train_array,test_array):
+        try:
+            logging.info("split train and split data")
+            X_train,y_train,X_test,y_test=(
+                train_array[:,:-1],
+                train_array[:,-1],
+                test_array[:,:-1],
+                test_array[:,-1]
+            )
+            models = {
+                "Random Forest": RandomForestRegressor(),
+                "Decision Tree": DecisionTreeRegressor(),
+                
+                "Linear Regression": LinearRegression(),
+                
+                
+                "AdaBoost Regressor": AdaBoostRegressor(),
+            }   
+            params={
+                "Decision Tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
+                },
+                "Random Forest":{
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                 
+                    # 'max_features':['sqrt','log2',None],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Gradient Boosting":{
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Linear Regression":{},
+                "XGBRegressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "CatBoosting Regressor":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "AdaBoost Regressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
+                
+            }
+   
+    
+            model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,
+                                             models=models,param=params)
+            
+
+            best_model_score=max(sorted(model_report.values()))
+            best_model_name=list(model_report.keys())[list(model_report.values()).index(best_model_score)]
+            best_model=models[best_model_name]
+            
+            if best_model_score<0.6:
+                raise CustomException("no best model found") 
+            logging.info(f"best model found on both training and test dataset")
+
+            save_object(
+                file_path=self.model_trainer_config.trained_model_file_path,
+                obj=best_model
+            )
+
+            
+            predicted=best_model.predict(X_test)
+            r2_scor=r2_score(y_test,predicted)
+            return r2_scor
+
+        except Exception as e:
+
+            raise CustomException(e,sys)   
+
